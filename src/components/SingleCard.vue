6ffd2679e67e6600aa67ead3efa44c378577f81c
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 16:39:49 +0200

    adding bootstrap card

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <p>Single Card</p>
+    <div class="card mb-3 col-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">
+                Titolo carta
+            </h5>
+            <p class="card-text">
+                Tipologia di carta
+            </p>
+        </div>
     </div>
 </template>
 <script>
