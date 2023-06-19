81ecaee9d47d260cf33b0278a00b6e05b8d46f9b
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 16:45:55 +0200

    import axios for management api

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -15,7 +15,7 @@
 
 <script>
 import SingleCard from './SingleCard.vue';
-
+import axios from 'axios';
 export default {
     name : 'CardList',
     data() {
@@ -26,7 +26,23 @@ export default {
 
     components: {
         SingleCard
-    }
+    },
+
+    created() {
+    // Make a request for a user with a given ID
+    axios.get('/user?ID=12345')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
+    },
 }
 </script>
 <style lang="scss" scoped>
