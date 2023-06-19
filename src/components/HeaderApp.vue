65b5d76197a8784ca6134da074bae71f885aedf4
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 16:25:35 +0200

    structuring the header with border and some content

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+    <header>
+        <div class="head">
+            <div class="logo">
+
+            </div>
+            <h1>Yu-Gi-Oh Api</h1>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
     name : 'HeaderApp',
+    data() {
+        return {
+            
+        }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
     
+    .head{
+        width: 100%;
+        border: 5px solid red;
+        padding: 10px 0;
+        display: flex;
+        align-items: center;
+
+        .logo{
+            border: 2px solid black;
+            width: 80px;
+            padding: 40px;
+            margin: 0 10px;
+        }
+
+    }
+
 </style>
