d2d338cfbf876d37c3828509607508f102438b75
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 18:30:36 +0200

    create a new component for waiting the load of card, add some style and html. add image

---
+++ src/components/LoaderApp.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container card-wrapper">
+        <img class="card" src="../assets/front.jpg" alt="" />
+        <img class="card card-back" src="../assets/back.jpg" alt="" />
+    </div>
+</template>
+<script>
+export default {
+    name : 'LoaderApp',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+        @keyframes cardFlip {
+    0% {
+        transform: rotateY(0deg);
+    }
+    50% {
+        transform: rotateY(180deg);
+    }
+    100% {
+        transform: rotateY(0deg);
+    }
+    }
+
+    .card-wrapper {
+    position: relative;
+    transform-style: preserve-3d;
+    animation: cardFlip 3s ease-in-out infinite;
+    width: 100px;
+    height: 150px;
+    }
+
+    .card {
+        position: absolute;
+        backface-visibility: hidden;
+        width: 100px;
+        height: 150px;
+    }
+
+    .card-back {
+        transform: rotateY(180deg);
+    }
+
+</style>
