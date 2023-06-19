36cdbd4c7e7f175be05a42ed4bcdb83618fe2876
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 16:13:10 +0200

    import CardList into MainApp

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        <h1>Main</h1>
-    </div>
+    <main>
+        <h1>
+            Main
+        </h1>
+        <CardList />
+    </main>
 </template>
 
 <script>
+
+import CardList from './CardList.vue';
+
+
 export default {
     name : 'MainApp',
+    data() {
+        return {
+            
+        }
+    },
+
+    components : {
+        CardList,
+    }
 }
 </script>
 
