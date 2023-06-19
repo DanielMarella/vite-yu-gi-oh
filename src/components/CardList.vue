f9676b23ea138f0e9d0814c9cdfa712f555efcf5
DanielMarella <marella.daniel96@gmail.com>
2023-06-19 16:12:29 +0200

    create 2 new component

---
+++ src/components/CardList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>
+            Card List
+        </h2>
+        <SingleCard/>
+    </div>
+</template>
+
+<script>
+import SingleCard from './SingleCard.vue';
+
+export default {
+    name : 'CardList',
+    data() {
+        return {
+            
+        }
+    },
+
+    components: {
+        SingleCard
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Single Card</p>
+    </div>
+</template>
+<script>
+export default {
+    name : 'SingleCard',
+    
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
