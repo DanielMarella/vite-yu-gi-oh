254afc1bdbae27f5739612e979f2140b058df9fe
DanielMarella <marella.daniel96@gmail.com>
2023-06-20 12:46:57 +0200

    move into right component the function 'created', adding props

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -7,12 +7,8 @@
                 </h2>
             </div>
         </div>
-        <div class="row" v-if="store.isLoading">
-            <div class="col-12">
-                <LoaderApp/>
-            </div>
-        </div>
-        <div class="row" v-else>
+
+        <div class="row">
             <SingleCard v-for="card in cardList"
             :cardImg="card.card_images[this.index].image_url"
             :cardName="card.name"
@@ -24,40 +20,27 @@
 
 <script>
 import SingleCard from './SingleCard.vue';
-import LoaderApp from './LoaderApp.vue';
-import axios from 'axios';
-import {store} from '../store.js'
+
+
+
 export default {
     name : 'CardList',
     data() {
         return {
-            cardList : [],
             index : 0,
-            store
         }
+    },
+
+    props : {
+        cardList : Array,  
     },
 
     components: {
     SingleCard,
-    LoaderApp
 },
-
-    created() {
-    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
-    .then( (response) => {
-        console.log(response.data.data);
-        this.cardList = response.data.data;
-        setTimeout( () => {
-            this.cardList = response.data.data;
-            this.store.isLoading = false;
-        },4000)
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    },
 }
 </script>
+
 <style lang="scss" scoped>
     h2{
         color: white;
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,7 +1,14 @@
 <template>
     <main>
         <SelectCardType />
-        <CardList />
+        <div class="row" v-if="store.isLoading">
+            <div class="col-12">
+                <LoaderApp/>
+            </div>
+        </div>
+        <div class="row" v-else >
+            <CardList :cardList="cardList" />
+        </div>
     </main>
 </template>
 
@@ -9,18 +16,38 @@
 
 import CardList from './CardList.vue';
 import SelectCardType from './SelectCardType.vue';
+import axios from 'axios';
+import LoaderApp from './LoaderApp.vue';
+import {store} from '../store.js'
 
 export default {
     name : 'MainApp',
     data() {
         return {
-            
+            store,
+            cardList : [],
         }
+    },
+
+    created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+    .then( (response) => {
+        console.log(response.data.data);
+        this.cardList = response.data.data;
+        setTimeout( () => {
+            this.cardList = response.data.data;
+            this.store.isLoading = false;
+        },2000)
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
     },
 
     components : {
         CardList,
         SelectCardType,
+        LoaderApp,
     }
 }
 </script>
