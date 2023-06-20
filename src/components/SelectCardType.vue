b1d3906d3a133abac08a06fb7167e2b1db3f11fc
DanielMarella <marella.daniel96@gmail.com>
2023-06-20 15:48:48 +0200

    add the option in the selection dinamically

--- src/components/SelectCardType.vue
+++ src/components/SelectCardType.vue
@@ -2,21 +2,34 @@
     <div class="container">
         <select class="form-select w-25 mb-5" aria-label="Default select example">
             <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option v-for="archetype in typeList"> {{ archetype.archetype_name}} </option>
         </select>
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
     name : 'SelectCardType',
 
     data() {
         return {
-            
+            typeApiUrl : 'https://db.ygoprodeck.com/api/v7/archetypes.php',
+            indexType : 5,
+            typeList : [],
         }
     },
+
+
+    created() {
+    axios.get(this.typeApiUrl)
+    .then( (type) => {
+        for (let i = 0; i < 20; i++){
+            this.typeList.push(type.data[i]);
+        }
+    })
+    },
+
 }
 </script>
 <style lang="scss" scoped>
